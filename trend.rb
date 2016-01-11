33c50e9f0dfd1c0b5eac3b0c3a4bcd544a9717bd
hegwin <zwt315@163.com>
2016-01-11 15:23:37 +0800

    Calculate and compare with previous MA5/20

--- trend.rb
+++ trend.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # compare MA5 & MA20
 
 require 'stock_quote'
@@ -10,10 +11,23 @@ prices = histories.map(&:adj_close)
 ma5 = prices[0..4].inject {|sum, price| sum + price } / 5
 ma20 = prices[0..19].inject {|sum, price| sum + price } / 20
 
+pma5 = prices[1..5].inject {|sum, price| sum + price } / 5
+pma20 = prices[1..20].inject {|sum, price| sum + price } / 20
+
 p "MA5: #{ma5} MA20: #{ma20}"
 if ma5 > ma20
   p "YES!!"
+  if pma5 < pma20
+    p "买入"
+  else
+    p "继续持仓"
+  end
 elsif ma5 < ma20
   p "NO!!"
+  if pma5 < pma20
+    p "保持空仓"
+  else
+    p "卖出"
+  end
 end
 
