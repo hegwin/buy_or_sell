2948e7ad5f6eeb98266618d12e0c1802db515d03
hegwin <zwt315@163.com>
2016-01-11 16:43:56 +0800

    Read stock symbols from file; Add date parameter

--- trend.rb
+++ trend.rb
@@ -3,31 +3,39 @@
 
 require 'stock_quote'
 
-code = '000883.SZ'
-
-histories = StockQuote::Stock.history(code, '2015-10-01', '2016-01-11')
-prices = histories.map(&:adj_close)
-
-ma5 = prices[0..4].inject {|sum, price| sum + price } / 5
-ma20 = prices[0..19].inject {|sum, price| sum + price } / 20
-
-pma5 = prices[1..5].inject {|sum, price| sum + price } / 5
-pma20 = prices[1..20].inject {|sum, price| sum + price } / 20
-
-p "MA5: #{ma5} MA20: #{ma20}"
-if ma5 > ma20
-  p "YES!!"
-  if pma5 < pma20
-    p "买入"
+def trend(ma5, ma20, pma5, pma20)
+  if ma5 > ma20
+    if pma5 < pma20
+      "建仓"
+    else
+      "继续持仓"
+    end
   else
-    p "继续持仓"
-  end
-elsif ma5 < ma20
-  p "NO!!"
-  if pma5 < pma20
-    p "保持空仓"
-  else
-    p "卖出"
+    if pma5 < pma20
+      "保持空仓"
+    else
+      "清仓"
+    end
   end
 end
 
+symbols = File.readlines('./portfolio.txt').map(&:chomp)
+results = {}
+end_date = ARGV[0] || Date.today.to_s
+
+symbols.each do |symbol|
+
+  histories = StockQuote::Stock.history(symbol, '2015-10-01', end_date)
+  prices = histories.map(&:adj_close)
+
+  ma5 = prices[0..4].inject {|sum, price| sum + price } / 5
+  ma20 = prices[0..19].inject {|sum, price| sum + price } / 20
+
+  pma5 = prices[1..5].inject {|sum, price| sum + price } / 5
+  pma20 = prices[1..20].inject {|sum, price| sum + price } / 20
+
+  results[symbol] = trend(ma5, ma20, pma5, pma20)
+
+end
+
+p results
