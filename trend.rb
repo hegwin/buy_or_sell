a022365f8c71d75cd694f7233f36dfd84214afb3
hegwin <zwt315@163.com>
2016-01-12 12:13:58 +0800

    Move result words to I18n

--- trend.rb
+++ trend.rb
@@ -1,20 +1,24 @@
-# encoding: utf-8
 # compare MA5 & MA20
 
 require 'stock_quote'
+require 'i18n'
+
+I18n.load_path = Dir['./i18n/*.yml']
+I18n.available_locales = [:en , :cn]
+#I18n.locale = :cn
 
 def trend(ma5, ma20, pma5, pma20)
   if ma5 > ma20
     if pma5 < pma20
-      "建仓"
+      :bid
     else
-      "继续持仓"
+      :possession
     end
   else
     if pma5 < pma20
-      "保持空仓"
+      :bear_position
     else
-      "清仓"
+      :ask
     end
   end
 end
@@ -34,8 +38,28 @@ symbols.each do |symbol|
   pma5 = prices[1..5].inject {|sum, price| sum + price } / 5
   pma20 = prices[1..20].inject {|sum, price| sum + price } / 20
 
-  results[symbol] = trend(ma5, ma20, pma5, pma20)
+  results[symbol] = I18n.t(trend(ma5, ma20, pma5, pma20))
 
 end
 
 p results
+
+__END__
+a = []
+0.upto(prices.size-20).each do |n|
+  ma5 = prices[n..(n+4)].inject {|sum, price| sum + price } / 5
+  ma20 = prices[n..(n+19)].inject {|sum, price| sum + price } / 20
+
+  pma5 = prices[(n+1)..(n+5)].inject {|sum, price| sum + price } / 5
+  pma20 = prices[(n+1)..(n+20)].inject {|sum, price| sum + price } / 20
+
+  t = trend(ma5, ma20, pma5, pma20)
+  if t.length <3
+    a <<[ t, prices[n]]
+  end
+
+
+end
+
+a.reverse!
+
