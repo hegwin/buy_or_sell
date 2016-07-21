3098cafe84e39cd2e3dffbf0b0307c44efef612e
hegwin <zwt315@163.com>
2016-07-21 19:01:00 +0800

    #4 Add action_mailer and config for sendgrid

--- Gemfile
+++ Gemfile
@@ -1,7 +1,10 @@
 source 'https://gems.ruby-china.org'
+ruby '2.2.3'
 
 gem 'sinatra'
 
+gem 'actionmailer', "~> 4.2.5", require: 'action_mailer'
 gem 'i18n'
 gem 'stock_quote'
+
 gem 'pry'
--- application.rb
+++ application.rb
@@ -1,5 +1,38 @@
 require 'sinatra'
+require 'action_mailer'
+
+class Mailer < ActionMailer::Base
+  default from: "Stock@Hegwin<notice@stock.hegwin.me>"
+
+  def notify(email)
+    mail to: email,
+         subject: 'Notification From Stock@Hegwin',
+         body: 'test'
+  end
+end
 
 get '/' do
   'Yea!~'
+end
+
+
+configure do
+  set :root,    File.dirname(__FILE__)
+  set :views,   File.join(Sinatra::Application.root, 'views')
+    
+  # ActionMailer::Base.view_paths = File.join(Sinatra::Application.root, 'views')
+
+  if production?
+    ActionMailer::Base.smtp_settings = {
+      :address => "smtp.sendgrid.net",
+      :port => '25',
+      :authentication => :plain,
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => ENV['SENDGRID_DOMAIN'],
+    }
+  else
+    ActionMailer::Base.delivery_method = :file
+    ActionMailer::Base.file_settings = { :location => File.join(Sinatra::Application.root, 'tmp/emails') }
+  end
 end
